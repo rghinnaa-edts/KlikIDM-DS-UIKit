d8fb109313f0a5a8a452075e43573f6cbd64492e
Rizka Ghinna Auliya <rizka.ghinna@sg-edts.com>
2025-02-26 13:11:14 +0700

    Initial Commit

---
+++ KlikIDM-DS-UiKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KlikIDM-DS-UiKit
+//
+//  Created by Rizka Ghinna Auliya on 26/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KlikIDM-DS-UiKitTests/KlikIDM_DS_UiKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  KlikIDM_DS_UiKitTests.swift
+//  KlikIDM-DS-UiKitTests
+//
+//  Created by Rizka Ghinna Auliya on 26/02/25.
+//
+
+import Testing
+@testable import KlikIDM_DS_UiKit
+
+struct KlikIDM_DS_UiKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
