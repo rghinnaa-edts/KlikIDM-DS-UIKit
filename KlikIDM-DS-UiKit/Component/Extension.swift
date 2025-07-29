a42eb7efc2e74570b2ea43fc8b46d173fabb810f
Rizka Ghinna Auliya <rizka.ghinna@sg-edts.com>
2025-07-29 13:58:55 +0700

    fix: extension

--- KlikIDM-DS-UiKit/Component/Extension.swift
+++ KlikIDM-DS-UiKit/Component/Extension.swift
@@ -6,87 +6,87 @@
 //
 
 import UIKit
+import ObjectiveC
 
 extension UIView {
-    
     private struct AssociatedKeys {
-        static var topLeftCornerRadius: UInt8 = 0
-        static var topRightCornerRadius: UInt8 = 0
-        static var bottomLeftCornerRadius: UInt8 = 0
-        static var bottomRightCornerRadius: UInt8 = 0
+        static let topLeftCornerRadius = UnsafeRawPointer(bitPattern: "topLeftCornerRadius".hashValue)!
+        static let topRightCornerRadius = UnsafeRawPointer(bitPattern: "topRightCornerRadius".hashValue)!
+        static let bottomLeftCornerRadius = UnsafeRawPointer(bitPattern: "bottomLeftCornerRadius".hashValue)!
+        static let bottomRightCornerRadius = UnsafeRawPointer(bitPattern: "bottomRightCornerRadius".hashValue)!
     }
-    
+
     var isTopLeftCornerRounded: Bool {
         get {
-            return (objc_getAssociatedObject(self, &AssociatedKeys.topLeftCornerRadius) as? Bool) ?? false
+            return (objc_getAssociatedObject(self, AssociatedKeys.topLeftCornerRadius) as? Bool) ?? false
         }
         set {
-            objc_setAssociatedObject(self, &AssociatedKeys.topLeftCornerRadius, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            objc_setAssociatedObject(self, AssociatedKeys.topLeftCornerRadius, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
             updateCornerMask()
         }
     }
-    
+
     var isTopRightCornerRounded: Bool {
         get {
-            return (objc_getAssociatedObject(self, &AssociatedKeys.topRightCornerRadius) as? Bool) ?? false
+            return (objc_getAssociatedObject(self, AssociatedKeys.topRightCornerRadius) as? Bool) ?? false
         }
         set {
-            objc_setAssociatedObject(self, &AssociatedKeys.topRightCornerRadius, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            objc_setAssociatedObject(self, AssociatedKeys.topRightCornerRadius, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
             updateCornerMask()
         }
     }
-    
+
     var isBottomLeftCornerRounded: Bool {
         get {
-            return (objc_getAssociatedObject(self, &AssociatedKeys.bottomLeftCornerRadius) as? Bool) ?? false
+            return (objc_getAssociatedObject(self, AssociatedKeys.bottomLeftCornerRadius) as? Bool) ?? false
         }
         set {
-            objc_setAssociatedObject(self, &AssociatedKeys.bottomLeftCornerRadius, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            objc_setAssociatedObject(self, AssociatedKeys.bottomLeftCornerRadius, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
             updateCornerMask()
         }
     }
-    
+
     var isBottomRightCornerRounded: Bool {
         get {
-            return (objc_getAssociatedObject(self, &AssociatedKeys.bottomRightCornerRadius) as? Bool) ?? false
+            return (objc_getAssociatedObject(self, AssociatedKeys.bottomRightCornerRadius) as? Bool) ?? false
         }
         set {
-            objc_setAssociatedObject(self, &AssociatedKeys.bottomRightCornerRadius, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            objc_setAssociatedObject(self, AssociatedKeys.bottomRightCornerRadius, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
             updateCornerMask()
         }
     }
-    
+
     private func updateCornerMask() {
         var cornerMask = CACornerMask()
-        
+
         if isTopLeftCornerRounded {
             cornerMask.insert(.layerMinXMinYCorner)
         }
-        
+
         if isTopRightCornerRounded {
             cornerMask.insert(.layerMaxXMinYCorner)
         }
-        
+
         if isBottomLeftCornerRounded {
             cornerMask.insert(.layerMinXMaxYCorner)
         }
-        
+
         if isBottomRightCornerRounded {
             cornerMask.insert(.layerMaxXMaxYCorner)
         }
-        
+
         self.layer.maskedCorners = cornerMask
     }
-    
+
     func setRoundedCorners(radius: CGFloat, corners: [UIRectCorner]) {
         self.layer.cornerRadius = radius
         self.layer.masksToBounds = true
-        
+
         self.isTopLeftCornerRounded = false
         self.isTopRightCornerRounded = false
         self.isBottomLeftCornerRounded = false
         self.isBottomRightCornerRounded = false
-        
+
         for corner in corners {
             switch corner {
             case .topLeft:
@@ -108,4 +108,3 @@ extension UIView {
         }
     }
 }
-
